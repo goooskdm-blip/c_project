c8af4d8e7d1c68d327bdf4a26de9e22ba692d93a
goooskdm <goooskdm@gmail.com>
2025-10-28 17:38:48 +0700

    first commit

---
+++ lab 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+
+int age = 15 ;
+char name[50]= "sarunphat";
+char gender = 'm';
+float GPA =4.00 ;
+
+    printf( "My Name is %s \n" , name) ; 
+    printf( "I'm %d \n" , age);
+    printf( "My Gender is %c \n" , gender ) ;
+    printf( "GPA is %f \n" , GPA) ;
+
+    return 0;
+
+
+
+
+}
